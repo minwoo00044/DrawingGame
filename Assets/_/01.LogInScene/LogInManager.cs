2e75d6af228a4b10449569fed1e727195922fae4
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2024-02-21 15:17:25 +0900

    playfab 로그인 구현
    로그인 매니저에 플레이팹 로그인, 사인업 기능 추가

--- Assets/_/01.LogInScene/LogInManager.cs
+++ Assets/_/01.LogInScene/LogInManager.cs
@@ -6,11 +6,19 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
+using System;
+using UnityEditor.PackageManager.Requests;
+
 
 public class LogInManager : MonoBehaviourPunCallbacks
 {
-    [SerializeField] TMP_InputField nickNameField;
+    [SerializeField] TMP_InputField emailField;
+    [SerializeField] TMP_InputField passwardField;
+    [SerializeField] TMP_InputField userNameField;
     [SerializeField] Button connectBtn;
+    [SerializeField] Button signhUpBtn;
     [SerializeField] GameObject LoadingBar;
     bool isConnected = false;
     private void Awake()
@@ -20,7 +28,9 @@ public class LogInManager : MonoBehaviourPunCallbacks
     // Start is called before the first frame update
     void Start()
     {
-        connectBtn.onClick.AddListener(Connect);
+
+        connectBtn.onClick.AddListener(LogIn);
+        signhUpBtn.onClick.AddListener(SignUp);
     }
 
     // Update is called once per frame
@@ -29,20 +39,65 @@ public class LogInManager : MonoBehaviourPunCallbacks
         
     }
 
-    void Connect()
+    void LogIn()
     {
         if(!isConnected)
         {
             Debug.Log("wait");
             return;
         }
-        PhotonNetwork.NickName = nickNameField.text;
-        Utils.SceneChange(SceneNum.Lobby);
+        var request = new LoginWithEmailAddressRequest { Email = emailField.text, Password = passwardField.text };
+        PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+
     }
+    void SignUp()
+    {
+        var request = new RegisterPlayFabUserRequest { Email = emailField.text, Password = passwardField.text, Username = userNameField.text };
+        PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnRegisterFailure);
+    }
+
     public override void OnConnectedToMaster()
     {
         Debug.Log("포톤접속");
         isConnected = true;
         LoadingBar.SetActive(false);
     }
+    private void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        Debug.Log("등록성공");
+        LogIn();
+    }
+    private void OnRegisterFailure(PlayFabError error)
+    {
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call!");
+
+        // PlayFabId로 GetAccountInfo API 호출
+        PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest
+        {
+            PlayFabId = result.PlayFabId
+        },
+        resultInfo =>
+        {
+            // API 호출 결과에서 UserName 가져오기
+            PhotonNetwork.NickName = resultInfo.AccountInfo.Username;
+            Utils.SceneChange(SceneNum.Lobby);
+        },
+        error =>
+        {
+            Debug.Log("Error getting account info: " + error.GenerateErrorReport());
+        });
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+
 }
